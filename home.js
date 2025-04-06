b01e2e9bdbb2522867c493a2e389bd94c5b78553
ColbyJohnson <colbjohnson025@gmail.com>
2025-04-06 02:19:40 -0400

    Please do the thing

--- home.js
+++ home.js
@@ -1,1 +1,11 @@
-alert('Hello, World!');
+import React from 'react'
+
+function App() {
+    return (
+      <div className="App">
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+  
+  export default App;
